952a92d156b46abf060c5e5cc9234448649931b8
= <=>
2019-04-15 01:14:51 -0300

    Fixed styles for itemlist and added shipping icon

--- frontend/src/ItemList/ItemRow.js
+++ frontend/src/ItemList/ItemRow.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import ShippingIcon from '../common/assets/ic_shipping.png';
 
 const ItemRow = ({item, onClick}) => {
     return(
-        <div className='item-list-row' key={item.id} onClick={() => onClick(item.id)} >
+        <div className='item-list-row' key={item.id} >
             <div className='item-info-first-col'>
-                <img src={item.picture} alt='itemImg' />
+                <img src={item.picture} alt='itemImg' onClick={() => onClick(item.id)} />
             </div>
             <div className='item-info-second-col'>
-                <span>{item.price}</span>
-                <span>{item.title}</span>
+                <div>
+                    <span className='item-price'>{item.price}</span>
+                    {item.free_shipping ? <img src={ShippingIcon} alt='icon-shipping' /> : null}
+                </div>
+                <span className='item-title' onClick={() => onClick(item.id)} >{item.title}</span>
             </div>
             <div className='item-info-third-col'>
                 <span>{item.city}</span>
